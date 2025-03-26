a64e4dbf36816a69e2712ce444967a0c319aeed3
no-merit <128347576+no-merit@users.noreply.github.com>
2025-03-26 18:15:14 +0900

    홈이 아닌 경로를 요청할 시 에러 출력

--- main.js
+++ main.js
@@ -6,19 +6,16 @@ const url = require('url');
 const app = http.createServer(function(request,response){
     let _url = request.url;
     let queryData = url.parse(_url,true).query;
-    console.log(queryData.id);
-    let title = queryData.id;
+    let pathname = url.parse(_url, true).pathname;
     //url 모듈의 parse 기능 사요
     //괄호 안에 있는 _url은 request.url 값을 가진 변수. 이 값을 분석해서 쿼리스티링 문자열만 추출하는 코드
-    if(_url == '/'){
-      title="Welcome";//요청받은 URL이 루트이면 실행
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
-
+    console.log(url.parse(_url, true));
+    
+   if(pathname ==='/'){
+    if(queryData.id===undefined){
+   
+      let title = 'Welcome';
+      let description = 'Hello, Node,js';
     
     let template =`
     <!doctype html>
@@ -39,7 +36,40 @@ const app = http.createServer(function(request,response){
 </body>
 </html>
 `;
+    response.writeHead(200);
+    response.end(template);
+   
+  }else{
+    fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
+      let title = queryData.id;
+      let description = 'Hello, Node,js';
+    
+    let template =`
+    <!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ul>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ul>
+  <h2>${title}</h2>
+  <p>${description}</p>
+</body>
+</html>
+`;
+    response.writeHead(200);
     response.end(template);
     });
+  }
+  }else{
+      response.writeHead(404);
+      response.end('Not found');
+    }
 });
 app.listen(80);
